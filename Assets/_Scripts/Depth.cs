ec1f7774f85a7fcca1894332a9699863e21cba18
Guillem Pujol Griñó <guille8.bcn@gmail.com>
2019-12-06 11:53:12 +0100

    new
    new

---
+++ Assets/_Scripts/Depth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Depth : MonoBehaviour
+{
+    public Transform target;
+    public Text depth;
+
+    void Update()
+    {
+        depth.text = Mathf.Round(target.position.y).ToString()+'m';
+    }
+}
+
